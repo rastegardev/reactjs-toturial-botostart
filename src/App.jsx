3e434d8729f7142a91568689f48581c5e79c0452
reza rastegar <moidreza82@gmail.com>
2023-03-29 16:27:50 +0330

    End - useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 import "./App.css";
 
+import Reducer1 from "./components/Reducer1";
+import Reducer2 from "./components/Reducer2";
+import Reducer3 from "./components/Reducer3";
+
+import DataState from "./pages/DataState";
+import DataReducer from "./pages/DataReducer";
+
 function App() {
-
-
   return (
     <>
-  
+      {/* <Reducer1 /> */}
+      {/* <Reducer2 /> */}
+      {/* <Reducer3 /> */}
+      {/* <DataState /> */}
+      {/* <DataReducer /> */}
     </>
   );
 }
---
+++ src/components/Reducer4.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reducer4 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Reducer4;
