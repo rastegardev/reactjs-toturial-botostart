0a7cd2caa6334a528bb776e7cdbeb8dca901173c
reza rastegar <moidreza82@gmail.com>
2023-04-02 02:21:10 +0330

    Custom Hooks - localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,34 @@ import React, { useState } from "react";
 import "./App.css";
 
 // import UseRef from "./components/UseRef";
-import LoginPage from "./components/LoginPage";
-import Landing from "./components/Landing";
+// import LoginPage from "./components/LoginPage";
+// import Landing from "./components/Landing";
+
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
+  // const [toggle, setToggle] = useState(true);
+
+  const [name, setName] = useLocalStorage("name", "");
 
   return (
     <>
       {/* <UseRef /> */}
-
-      {toggle ? <Landing /> : <LoginPage />}
+      {/* {toggle ? <Landing /> : <LoginPage />}
       <button
         onClick={() => {
           setToggle(!toggle);
         }}
       >
         Toggle
-      </button>
+      </button> */}
+
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="type here"
+      />
     </>
   );
 }
